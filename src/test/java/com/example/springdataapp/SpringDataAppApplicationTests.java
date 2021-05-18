b521fe2554999d1a52ad9920528d4800511d13fc
Tatiana Nosova <tatiananosova746@gmail.com>
2021-05-18 12:33:17 -0500

    Initial commit

---
+++ src/test/java/com/example/springdataapp/SpringDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdataapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
