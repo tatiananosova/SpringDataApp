8eccbcd478fa560591b6c8d880da5830c7470462
Tatiana Nosova <tatiananosova746@gmail.com>
2021-05-18 14:03:37 -0500

    Homework7

---
+++ src/main/java/com/example/springdataapp/model/Product.java
@@ -0,0 +1,24 @@
+package com.example.springdataapp.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "cost")
+    private int cost;
+}
+
---
+++ src/main/java/com/example/springdataapp/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.example.springdataapp.repository;
+
+import com.example.springdataapp.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByTitle(String title);
+    List<Product> findAllByCostBetween(int min, int max);
+    List<Product> findAllByCostAfter(int cost);
+    List<Product> findAllByCostBefore(int cost);
+    List<Product> findAllByTitleIsContaining(String titleContains);
+
+}
